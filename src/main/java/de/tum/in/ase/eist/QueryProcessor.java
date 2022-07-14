fdca1d4f0d1e2686160409cfbf80f255244a2bdc
Lucas Schönberger <lucas-schoenberger@web.de>
2022-07-14 13:01:23 +0200

    main

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,6 +13,19 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Lucas";
+        } else if (query.contains("largest")) {
+            String querystring = query.toString();
+            String[] parts = querystring.split(":");
+            String numbers = parts[1];
+            String[] newStr = numbers.split(",");
+            int largestnumber = 0;
+            for (String s : newStr) {
+                if (Integer.parseInt(s) > largestnumber) {
+                    largestnumber = Integer.parseInt(s);
+                }
+
+            }
+            return String.valueOf(largestnumber);
         } else { // TODO extend the programm here
             return "";
         }
